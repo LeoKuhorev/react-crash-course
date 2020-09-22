47169d202c9994dc664fcb12a290a4adff1f68c5
Leo Kukharau <kuhorev.leonid@gmail.com>
2020-09-22 16:34:17 -0700

    initial comit

---
+++ src/components/layout/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header style={headerStyle}>
+      <h1>ToDo List</h1>
+      <Link to="/" style={linkStyle}>
+        Home
+      </Link>{" "}
+      |{" "}
+      <Link to="/about" style={linkStyle}>
+        About
+      </Link>
+    </header>
+  );
+}
+
+export default Header;
+
+const linkStyle = {
+  color: "#fff",
+};
+
+const headerStyle = {
+  background: "#333",
+  color: "#fff",
+  textAlign: "center",
+  padding: "10px",
+};
